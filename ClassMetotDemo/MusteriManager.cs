53d662cbf12ae6bd2176f58a81071d92f1fcda6d
price34 <58705333+price34@users.noreply.github.com>
2021-01-19 21:35:13 +0300

    Update MusteriManager.cs

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,19 @@ namespace ClassMetotDemo
 {
     class MusteriManager
     {
+        public void Ekle(string musteriAdi, string musteriSoyadi, int hesapNo)
+        {
+            Console.WriteLine(musteriAdi + " " + musteriSoyadi + " adlı şahıs; " + hesapNo + " hesap numarası ile yeni müşteri olarak eklendi!");
+        }
+
+        public void Sil(Musteri m)
+        {
+            Console.WriteLine(m.hesapNo + " hesap numaralı, " + m.musteriAdi + " " + m.musteriSoyadi + " adlı müşteri silindi!");
+        }
+
+        public void Listele(Musteri m)
+        {
+            Console.WriteLine("Listelenen müşteri Bilgisi aşağıdaki gibidir!" + "\nAd - Soyad: " + m.musteriAdi + " " + m.musteriSoyadi + "\nHesap No: " + m.hesapNo + "\nBakiye: " + m.musteriBakiye + "TL");
+        }
     }
 }
